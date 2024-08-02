7049798bda231db454dee788fe24c205e0d5fd2b
Chathuri bhagya <148967016+chathubhagya@users.noreply.github.com>
2024-08-02 10:04:26 +0530

    add js file

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var typed=new Typed(".Text",{
+    strings:["Front end developer","full stack developer","Software Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
